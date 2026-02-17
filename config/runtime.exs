4a44f0cf5166e047857db545dc8111d39d0b26f2
Scott Sproule <scott.sproule@ficonab.com>
2026-02-16 22:53:50 -0800

    update for email access

--- config/runtime.exs
+++ config/runtime.exs
@@ -59,8 +59,8 @@ if config_env() == :prod do
 
    config :shelley_plants, ShelleyPlants.Mailer,
     adapter: Swoosh.Adapters.AmazonSES,
-    access_key_id: System.get_env("AWS_ACCESS_KEY_ID"),
-    secret_access_key: System.get_env("AWS_SECRET_ACCESS_KEY"),
+    access_key: System.get_env("AWS_ACCESS_KEY_ID"),
+    secret: System.get_env("AWS_SECRET_ACCESS_KEY"),
     region: "us-east-1"
 
 
