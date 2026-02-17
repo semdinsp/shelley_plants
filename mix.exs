65ef84d0e3b40ae684a09a4614067604b2764d93
Scott Sproule <scott.sproule@ficonab.com>
2026-02-16 22:56:50 -0800

    update for email

--- mix.exs
+++ mix.exs
@@ -60,6 +60,7 @@ defmodule ShelleyPlants.MixProject do
        compile: false,
        depth: 1},
       {:swoosh, "~> 1.16"},
+      {:gen_smtp, "~> 1.0"},
       {:req, "~> 0.5"},
       {:telemetry_metrics, "~> 1.0"},
       {:telemetry_poller, "~> 1.0"},
